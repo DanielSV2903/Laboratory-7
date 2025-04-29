ef532ec37b6e44defa672ab0eb315491efb8f3b4
Daniel Sánchez Vargas <danielpat433@gmail.com>
2025-04-28 23:01:07 -0600

    La mayoria de metodos sirven, algunos test implementados

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,57 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.laboratory7.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Text txtMessage;
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+
+    private void load(String form) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(form));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void Home(ActionEvent actionEvent) {
+        this.txtMessage.setText("Laboratory 7\nDaniel Sánchez Vargas\nAndreé Murillo Sojo\nAidan Murillo Corrales");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    public void Exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    @FXML
+    public void exampleOnMousePressed(Event event) {
+        this.txtMessage.setText("Loading Example. Please wait!!!");
+    }
+
+    @FXML
+    public void arithmeticConverterOnAction(ActionEvent actionEvent) {
+        load("arithmeticConverter.fxml");
+    }
+
+    @FXML
+    public void baseConverterOnAction(ActionEvent actionEvent) {
+        load("baseConverter.fxml");
     }
 }
