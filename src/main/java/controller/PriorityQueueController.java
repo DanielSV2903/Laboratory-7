aea1de3bb64dac26aa6534828160495eaa23a463
Daniel Sánchez Vargas <danielpat433@gmail.com>
2025-05-01 11:09:40 -0600

    UI

--- src/main/java/controller/PriorityQueueController.java
+++ src/main/java/controller/PriorityQueueController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
 public class PriorityQueueController {
+    @javafx.fxml.FXML
+    private TextArea tArea;
+    @javafx.fxml.FXML
+    private TableView tView;
+    @javafx.fxml.FXML
+    private TableColumn tColPriority;
+    @javafx.fxml.FXML
+    private ChoiceBox cBoxMood;
+    @javafx.fxml.FXML
+    private TextField tfName;
+    @javafx.fxml.FXML
+    private TableColumn tColMood;
+    @javafx.fxml.FXML
+    private TableColumn tColAttention;
+    @javafx.fxml.FXML
+    private ChoiceBox cBoxPriority;
+    @javafx.fxml.FXML
+    private TableColumn tColName;
+    @javafx.fxml.FXML
+    private BorderPane bp;
+
+    @javafx.fxml.FXML
+    public void enQueueOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
 }
